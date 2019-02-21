a926d29cf4bf5435eb240f30e49fc460a66bb31d
孙银龙 <sunyinlong@outlook.com>
2019-02-21 19:39:38 +0800

    孙银龙 用户评价 地图开发

--- LX-Ordering/Controllers/ClientController.cs
+++ LX-Ordering/Controllers/ClientController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LX_Ordering.Models;
+using Newtonsoft.Json;
 
 namespace LX_Ordering.Controllers
 {
@@ -62,19 +63,45 @@ namespace LX_Ordering.Controllers
         }
         //查看商品的所有评价（）
 
-        public ActionResult SingleA()
+        public ActionResult SingleA(int Did = 0, int PageIndex = 1, int PageSize = 3)
         {
-            return View();
+            //获取评价信息
+            List<EvaluateInfo> evaluatelist = CommonGet<EvaluateInfo>.GetList();
+            //模糊查询
+            if (Did > 0)
+            {
+                evaluatelist = (from a in evaluatelist where a.Did.Equals(Did) select a).ToList();
+            }
+            //总记录数
+            int Count = evaluatelist.Count();
+            //总页数
+            ViewBag.PageCount = Math.Ceiling(Count * 1.0 / PageSize);
+            ViewBag.PageIndex = PageIndex;
+            ViewBag.Count = Count;
+            ViewBag.PageSize = PageSize;
+            return View(evaluatelist.Skip((PageIndex - 1) * PageSize).Take(PageSize));
         }
         [HttpGet]//添加评价
         public ActionResult SingleAdd()
-        {
+        {          
             return View();
         }
         [HttpPost]//添加评价
-        public ActionResult SingleAdd(DishInfo dish)
+        public void SingleAdd(EvaluateInfo evaluate)
         {
-            return View();
+            //评价时间
+            evaluate.ETime = DateTime.Now;
+            var url = "api/OrderAPI/AddEvaluate";
+            string data = JsonConvert.SerializeObject(evaluate);
+            int result = Convert.ToInt32(HttpClientHelper.SendRequest(url, "post", data));
+            if (result > 0)
+            {
+                Response.Write("<script>alert('评价成功');location.href='/Client/SingleA';</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('评价失败')</script>");
+            }
         }
         //反填
         [HttpGet]
@@ -87,6 +114,12 @@ namespace LX_Ordering.Controllers
         public ActionResult UpInfo(ClientInfo client)
         {
             return View();
+        }
+        //地图方法
+        public ActionResult Map()
+        {   
+            List<Logistics> logisticslist = CommonGet<Logistics>.GetList();           
+            return View(logisticslist);
         }
     }
 }
